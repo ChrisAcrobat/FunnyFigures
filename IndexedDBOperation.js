64d58456a0826f0fcb8e8c86c449fdece1151367
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-12-13 18:07:18 +0100

    Added IndexedDBOperation.

---
+++ IndexedDBOperation.js
@@ -0,0 +1,23 @@
+class IndexedDBOperation {
+	static do = call => {
+		let worker = new Worker('indexedDB.js');
+		let resolve;
+		let reject;
+		let promise = new Promise((_resolve, _reject) => {resolve = _resolve; reject = _reject;});
+		let awaitingResponse = true;
+		worker.onmessage = m => {
+			if(awaitingResponse){
+				awaitingResponse = false;
+				resolve(m.data);
+			}
+		}
+		worker.onerror = e => {
+			if(awaitingResponse){
+				awaitingResponse = false;
+				reject(e);
+			}
+		}
+		worker.postMessage(call);
+		return promise;
+	}
+}
